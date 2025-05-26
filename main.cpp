99769f811b23560f4109f2c89d9b6d0a09eaa93d
VictorMiguelMachado <164941822+VictorMiguelMachado@users.noreply.github.com>
2025-05-25 23:08:44 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct product{
+    string nameProduct;
+    float price;
+    int codeProduct;
+    int quantityProduct;
+
+} structProducts;
+
+int main(){
+
+    int opcao;
+    vector<product> produtos;
+
+    do{
+        product newProduct;
+
+        cout << "Escolha uma opcao: " << endl;
+        cout << "1 - cadastrar produto" << endl;
+        cout << "2 - Listar produtos" << endl;
+        cout << "3 - Buscar por nome" << endl;
+        cout << "4 - sair" << endl; 
+        cin >> opcao;
+        cin.ignore();
+
+        switch (opcao){
+        case 1:
+
+            cout << "Digite o nome do produto: " << endl;
+            getline(cin, newProduct.nameProduct);
+
+            cout << "Digite o codigo do produto: " << endl;
+            cin >> newProduct.codeProduct;
+
+            cout << "Digite o preco do produto: " << endl;
+            cin >> newProduct.price;
+            
+            cout << "Digite a quantidade em estoque: " << endl;
+            cin >> newProduct.quantityProduct;
+
+            produtos.push_back(newProduct);
+        break;
+        
+        default:
+            cout << "Opcao Invalida!" << endl;
+            break;
+        }
+    } while (opcao != 4);
+
+    return 0;
+}
