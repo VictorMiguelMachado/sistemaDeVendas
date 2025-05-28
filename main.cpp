e404d559698d4b85d041bff334b2478012d5e4a0
VictorMiguelMachado <164941822+VictorMiguelMachado@users.noreply.github.com>
2025-05-28 20:11:12 -0300

    Add files via upload
    Este é um sistema básico para gerenciamento de produtos e vendas, desenvolvido em C++. O programa permite cadastrar produtos, listar os produtos cadastrados, buscar um produto pelo nome e realizar vendas com controle de estoque.
    
    Funcionalidades: 
    - Cadastro de produtos com nome, código, preço e quantidade em estoque.
    - Listagem completa dos produtos cadastrados.
    - Busca de produtos pelo nome.
    - Realização de vendas, atualizando a quantidade em estoque e acumulando o total vendido.
    - Exibição do total de vendas ao encerrar o programa.
    
    Como usar?
    Basta compilar e executar o programa. Ele apresentará um menu para interagir com as opções disponíveis.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,12 +10,17 @@ struct product{
     float price;
     int codeProduct;
     int quantityProduct;
+    int quantityProductV;
 
 } structProducts;
+
+
 
 int main(){
 
     int opcao;
+    string nomeBuscado;
+    float totalVendas = 0;
     vector<product> produtos;
 
     do{
@@ -24,7 +30,8 @@ int main(){
         cout << "1 - cadastrar produto" << endl;
         cout << "2 - Listar produtos" << endl;
         cout << "3 - Buscar por nome" << endl;
-        cout << "4 - sair" << endl; 
+        cout << "4 - Realizar venda" << endl;
+        cout << "5 - sair" << endl; 
         cin >> opcao;
         cin.ignore();
 
@@ -44,6 +51,7 @@ int main(){
             cin >> newProduct.quantityProduct;
 
             produtos.push_back(newProduct);
+            cin.ignore();
         break;
         case 2:
             cout << "Lista de produtos: " << endl;
@@ -55,12 +63,70 @@ int main(){
                 cout << "Quantidade: " << produtos[i].quantityProduct << endl;
                 cout << "-------------------------------------------------------" << endl;
             }
+            
         break;
+        case 3:{
+            
+            cout << "Digite o nome do produto: " << endl;
+            getline(cin, nomeBuscado);
+            
+            auto it = find_if(produtos.begin(), produtos.end(), [&](const product& p) {
+            return p.nameProduct == nomeBuscado;
+        });
+
+         if (it != produtos.end()) {
+            cout << "Produto encontrado:" << endl;
+            cout << "Nome: " << it->nameProduct << endl;
+            cout << "Codigo: " << it->codeProduct << endl;
+            cout << "Preco: " << it->price << endl;
+            cout << "Quantidade: " << it->quantityProduct << endl;
+        } else {
+            cout << "Produto nao encontrado." << endl;
+        }
+
+        break;
+        cin.ignore();
+    }
+    case 4:{
+            cout << "Digite o nome do produto para comprar: " << endl;
+            getline(cin, nomeBuscado);
+            
+            auto it = find_if(produtos.begin(), produtos.end(), [&](const product& p) {
+                return p.nameProduct == nomeBuscado;
+            });
+
+            if (it != produtos.end()) {
+                int quantidadeComprar;
+                cout << "Digite a quantidade que deseja comprar: " << endl;
+                cin >> quantidadeComprar;
+                
+                if(quantidadeComprar <= it->quantityProduct){
+                    it->quantityProduct -= quantidadeComprar;
+                    float valorVenda = quantidadeComprar * it->price;
+                    totalVendas += valorVenda;
+
+                    cout << "Produto vendido com sucesso!" << endl;
+                    cout << "Quantidade comprada: " << quantidadeComprar << endl;
+                    cout << "Valor total da compra: R$ " << valorVenda << endl;
+                }else{
+                    cout << "Quantidade indisponivel em estoque!" << endl;                
+                }
+                cout << "Produto nao encontrado!" << endl;
+        }
+        break;
+    }
+
+    case 5:
+
+        cout << "Encerrando programa" << endl;
+        cout << "Total de vendas do dia: RS$ " << totalVendas << endl;
+        break;
+        
         default:
             cout << "Opcao Invalida!" <<  endl;
             break;
         }
-    } while (opcao != 4);   
+    } while (opcao != 5);   
 
     return 0;
 }
